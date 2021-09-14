357b8a5d72cecc2a375a94a1e89ccdb2a06e5ed6
Ashwin.D.S <astroashwin@outlook.com>
2021-09-14 13:12:24 +0400

    VoTeApp_py: fixup firebase realtime db connection

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# Import database module.
-import firebase_admin
-from firebase_admin import db
-from firebase_admin import credentials
-firebase_admin.initialize_app()
-firebase_instance = firebase_admin.get_app()
-default_app = firebase_admin.initialize_app(firebase_admin.credentials.Certificate("google-services.json"), {'databaseURL':"https://voteapp-1e334-default-rtdb.asia-southeast1.firebasedatabase.app"})
-
-
-# Get a database reference to our posts
-ref = db.reference('https://voteapp-1e334-default-rtdb.asia-southeast1.firebasedatabase.app')
-# Read the data at the posts reference (this is a blocking operation)
-print(ref.get())
+import requests
+import json
+authkey = open("authkey.txt", "r").read()
+get = requests.get(authkey)
+json_parsed = json.loads(get.content)
+noOfCandidates = 1
+for candidate in json_parsed:
+    print(noOfCandidates)
+    print (json_parsed[candidate]['name'])
+    print (json_parsed[candidate]['desc'])
+    noOfCandidates+=1
